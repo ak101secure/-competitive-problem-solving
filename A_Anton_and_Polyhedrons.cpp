9da234eed06b0fa0db1b94b8732801c155cdb636
afzal <afzal@gmail.com>
2025-07-16 19:33:11 +0600

    file add

--- A_Anton_and_Polyhedrons.cpp
+++ A_Anton_and_Polyhedrons.cpp
@@ -4,17 +4,16 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-
     string polyhedrons[] = {
+
         "Tetrahedron",
         "Cube",
         "Octahedron",
         "Dodecahedron",
         "Icosahedron"
+
     };
-
     int ans = 0; 
-
     for (int i = 0; i < n; i++) {
         string v;
         cin >> v;
