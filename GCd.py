f0367ac775f968acdf923a259f64a31bb4595e21
afzal <afzal@gmail.com>
2025-07-16 15:45:13 +0600

    file add

---
+++ GCd.py
@@ -0,0 +1,16 @@
+
+def gcd(a,b):
+
+    minvalue = min(a,b)
+
+    while(minvalue>0):
+        if(a%minvalue ==0) and (b%minvalue ==0):
+            return minvalue 
+        minvalue = minvalue - 1 
+        
+    else:
+        return -1 
+
+if __name__ =="__main__":
+
+    print(gcd(36,60))
