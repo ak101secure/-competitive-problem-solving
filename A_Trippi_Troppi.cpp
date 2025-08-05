7e8c1ac626f70ef6657d63f73039ad13dbc7322a
afzal <afzal@gmail.com>
2025-08-06 02:00:53 +0600

    file add

---
+++ A_Trippi_Troppi.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define SQUARE(x) (x*x)
+
+int main(){
+
+    int n = 7 ; 
+    int result = SQUARE(n);
+
+    cout<<"The result of SQUARE :" <<result<<endl;
+
+ return 0;
+
+}
