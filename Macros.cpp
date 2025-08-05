8b49f29cc8ca0094130c507392e336812a1ca422
afzal <afzal@gmail.com>
2025-08-06 03:02:51 +0600

    add files

---
+++ Macros.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+
+int main() {
+    vector<int>vect ;
+    vect.push_back(543);
+    vect.push_back(434343);
+    vect.push_back(332344);
+    vect.push_back(333);
+
+    for(auto x : v){
+        cout<<x<<endl;
+    }
+
+    return 0;
+}
